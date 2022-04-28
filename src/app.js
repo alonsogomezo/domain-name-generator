2309cae64588971ea07964d6e3bb7f1e341fb0f1
alonsogomezo <luisalonsogo95@gmail.com>
2022-04-28 02:55:10 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+var pronoun = ["the", "our"];
+
+var adj = ["great", "big"];
+
+var noun = ["jogger", "racoon"];
+
+for (var i = 0; i < pronoun.length; i++) {
+  for (var x = 0; x < pronoun.length; x++) {
+    for (var j = 0; j < pronoun.length; j++) {
+      console.log(pronoun[i] + adj[x] + noun[j] + ".com");
+    }
+  }
+}
